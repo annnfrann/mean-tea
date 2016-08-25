acbcb7c82a66aee7297723d7b4378560db8e4cc6
Anna Lewis <anna.frances.lewis@gmail.com>
2016-08-25 17:31:56 -0600

    duplicates do not add to cart

--- js/storefrontController.js
+++ js/storefrontController.js
@@ -6,49 +6,26 @@ app.controller('storefrontController', function($scope, $http, CartFactory){
   $scope.getAllTeas = $http.get('tea.json').then(function(data){
     $scope.view.allTeas = data.data
   })
-  $scope.addToCart = function(teaName, teaImage, teaCaffeine, teaIngredients, teaRating, teaPrice, teaQuantity){
-    if (CartFactory.cartTeas.length != 0) {
-      // change this from hard coded as index position zero
-      console.log(CartFactory.cartTeas);
-      console.log($scope.teasInCart);
-      if (CartFactory.cartTeas[0].name == $scope.teasInCart.name) {
-        console.log("OMFG MATCHERS!");
-        teaQuantity = Number(teaQuantity);
-        CartFactory.cartTeas[0].quantity += teaQuantity
-        console.log(CartFactory.cartTeas[0].quantity);
-        // only increase quantity
-      }else {
-        $scope.teasInCart = {}
-        $scope.teasInCart.name = teaName
-        $scope.teasInCart.image = teaImage
-        $scope.teasInCart.caffeine = teaCaffeine
-        $scope.teasInCart.ingredients = teaIngredients
-        $scope.teasInCart.rating = teaRating
-        $scope.teasInCart.price = teaPrice
-        $scope.teasInCart.quantity = teaQuantity
-        CartFactory.cartTeas.push($scope.teasInCart)
-      }
-    }else {
-      $scope.teasInCart = {}
-      $scope.teasInCart.name = teaName
-      $scope.teasInCart.image = teaImage
-      $scope.teasInCart.caffeine = teaCaffeine
-      $scope.teasInCart.ingredients = teaIngredients
-      $scope.teasInCart.rating = teaRating
-      $scope.teasInCart.price = teaPrice
-      $scope.teasInCart.quantity = teaQuantity
-      CartFactory.cartTeas.push($scope.teasInCart)
+  $scope.addToCart = function(tea){
+    if (!tea.quantity) {
+      tea.quantity = 1
+    } else {
+      tea.quantity = Number(tea.quantity)
     }
-    // for loop -> if CartFactory.cartTeas[i].name == $scope.teasInCart.name
-    // add $scope.teasInCart.quantity to CartFactory.cartTeas[i].quantity
-
-
-    // add these to an object, and then push the object into the array in the factory
-
-    $scope.view.cartTotal = CartFactory.cartQuantity.number += Number(teaQuantity)
-
-
-    console.log("LOOK: ", $scope.teasInCart.name);
-    console.log("LOOK: ", $scope.teasInCart.quantity);
+    if(!CartFactory.cartTeas.length){
+      CartFactory.cartTeas.push(tea);
+    } else {
+      for (var i = 0; i < CartFactory.cartTeas.length; i++) {
+        if (CartFactory.cartTeas.indexOf(tea) === -1) {
+          console.log("AHAHHAHAHA");
+          CartFactory.cartTeas.push(tea);
+          break
+        } else {
+          CartFactory.cartTeas[i].quantity += (Number(tea.quantity) + 1)
+          // find out why you had to add one more?
+          break
+        }
+      }
+    console.log(CartFactory.cartTeas);
   }
 })
