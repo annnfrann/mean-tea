1dad259f828f5378677ef1c27e8579f6876530e7
Anna Lewis <anna.frances.lewis@gmail.com>
2016-08-26 15:55:21 -0600

    user can use full site

--- js/shoppingcartController.js
+++ js/shoppingcartController.js
@@ -1,8 +1,27 @@
 app.controller('shoppingcartController', function($scope, CartFactory){
+  $scope.quantities = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   $scope.view = {}
   $scope.view.teas = CartFactory.cartItems()
-  console.log($scope.view.teas)
-  // get cartItems from the CartFactory and then iterate through those to populate the page
-  // add the functionality to edit the quantity of teas ordered
-  //also, get all the totals and add those together for the subtotal
+  function orderTotal(total, num) {
+    return total + num;
+  }
+  var prices = []
+  for (var i = 0; i < $scope.view.teas.length; i++) {
+    prices.push(($scope.view.teas[i].quantity*$scope.view.teas[i].price)/100)
+  }
+  function getOrderTotal(item) {
+    var total = prices.reduce(orderTotal);
+    return total
+  }
+  $scope.view.orderTotal = getOrderTotal()
+
+  $scope.removeFromCart = function(input){
+    $scope.view.orderTotal = $scope.view.orderTotal - ($scope.view.teas[input].price / 100)
+    $scope.view.teas.splice(input, 1)
+  }
+  $scope.updateQuantity = function(index, newQuantity){
+    var oldPrice = $scope.view.teas[index].price/100
+    $scope.view.teas[index].quantity = newQuantity
+    $scope.view.orderTotal = $scope.view.orderTotal - oldPrice + (newQuantity * $scope.view.teas[index].price) /100
+  }
 })
